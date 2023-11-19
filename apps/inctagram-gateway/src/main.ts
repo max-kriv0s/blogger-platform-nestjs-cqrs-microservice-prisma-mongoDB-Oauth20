72ab66462e896613dd5c589ebeda7200734cf352
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-19 19:25:21 +0300

    I1-41 refactor: add prisma module

--- apps/inctagram-gateway/src/main.ts
+++ apps/inctagram-gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { configApp } from './core/config';
+import { configApp } from './core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
