9ee969a2c1983c701bc4dac1b7a63310d26e2f05
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-19 19:17:10 +0300

    I1-41 build: add settings swagger, pipes, cookie parser

---
+++ apps/inctagram-gateway/src/core/config/swagger/swaggerSetup.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Intagram')
+    .setDescription('The intagram API description')
+    .setVersion('1.0')
+    .addTag('Intagram')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+}
--- apps/inctagram-gateway/src/main.ts
+++ apps/inctagram-gateway/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { configApp } from './core/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000);
+
+  configApp(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 
 bootstrap();
