f2b135237f30aef88b45b65f27ec69b46f7bfbef
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-22 00:12:29 +0300

    I1-46 refactor: pipesSetup and add enableCors

--- apps/inctagram-gateway/src/core/config/configApp.ts
+++ apps/inctagram-gateway/src/core/config/configApp.ts
@@ -1,10 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { swaggerSetup } from './swagger/swaggerSetup';
 import cookieParser from 'cookie-parser';
+import { pipesSetup } from './pipes/pipesSetup';
 
 export function configApp(app: INestApplication) {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+  app.enableCors();
 
+  pipesSetup(app);
   swaggerSetup(app);
 }
