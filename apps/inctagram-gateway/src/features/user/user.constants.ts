8e09fb36b1dfc967d3b118c048cfa8b3a57a7b8d
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-23 23:37:52 +0300

    I1-46 feat: add user constant

---
+++ apps/inctagram-gateway/src/features/user/user.constants.ts
@@ -0,0 +1,13 @@
+export const ERROR_FORMAT_EMAIL =
+  'The email must match the format example@example.com';
+
+export const ERROR_LENGTH_USERNAME = `
+{Minimum number of characters 6}
+{Maximum number of characters 20}`;
+
+export const ERROR_LENGTH_PASSWORD = `
+{Minimum number of characters 6}
+{Maximum number of characters 20}`;
+
+export const ERROR_PASSWORDS_MUST_MATCH = 'Passwords must match';
+export const ERROR_PASSWORD_MUST_CONTAIN = `Password must contain 0-9, a-z, A-Z, ! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _\` \{ \| \} ~`;
