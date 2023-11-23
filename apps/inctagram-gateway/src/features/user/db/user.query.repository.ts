57813556a5867a51a13ed4f464c1e7248dd44417
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-24 01:37:07 +0300

    I1-46 feat: add result  and findByUsernameOrEmail

--- apps/inctagram-gateway/src/features/user/db/user.query.repository.ts
+++ apps/inctagram-gateway/src/features/user/db/user.query.repository.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../core/prisma/prisma.servise';
 import { ResponseUserDto } from '../responses';
+import { Result } from '../../../core/result';
+import { NotFoundError } from '../../../core/exceptions';
+import { USER_NOT_FOUND } from '../user.constants';
 
 @Injectable()
 export class UserQueryRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getUserViewById(id: string): Promise<ResponseUserDto | null> {
+  async getUserViewById(id: string): Promise<Result<ResponseUserDto>> {
     const user = await this.prismaService.user.findUnique({
       where: { id, isDeleted: false },
     });
     if (!user) {
-      return null;
+      return Result.Err(new NotFoundError(USER_NOT_FOUND));
     }
-    return ResponseUserDto.getView(user);
+    return Result.Ok(ResponseUserDto.getView(user));
   }
 }
