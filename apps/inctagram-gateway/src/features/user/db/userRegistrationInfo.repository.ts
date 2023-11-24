fd4bcb6f502badddf81a004e59c8d7622ced2df0
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-25 00:17:41 +0300

    I1-46 feat: add ConfirmationRegistrationUseCase

--- apps/inctagram-gateway/src/features/user/db/userRegistrationInfo.repository.ts
+++ apps/inctagram-gateway/src/features/user/db/userRegistrationInfo.repository.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../core/prisma/prisma.servise';
 import { CreateUserInfoDto } from '../dto';
+import { UserRegistrationInfo } from '@prisma/client';
 
 @Injectable()
 export class UserRegistrationInfoRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(userInfo: CreateUserInfoDto) {
+  async create(userInfo: CreateUserInfoDto): Promise<UserRegistrationInfo> {
     return this.prismaService.userRegistrationInfo.create({
       data: {
         userId: userInfo.userId,
         confirmationCode: userInfo.confirmationCode,
         expirationConfirmationCode: userInfo.expirationConfirmationCode,
       },
+    });
+  }
+
+  update(
+    id: string,
+    data: Partial<Omit<UserRegistrationInfo, 'id'>>,
+  ): Promise<UserRegistrationInfo> {
+    return this.prismaService.userRegistrationInfo.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async findUserByCodeConfirmation(
+    code: string,
+  ): Promise<UserRegistrationInfo | null> {
+    return this.prismaService.userRegistrationInfo.findFirst({
+      where: { confirmationCode: code },
     });
   }
 }
