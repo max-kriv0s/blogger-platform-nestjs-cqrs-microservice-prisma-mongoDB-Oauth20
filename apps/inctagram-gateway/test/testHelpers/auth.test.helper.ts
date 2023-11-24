8bbdf42141405ddc8b940861da7d3c5471e8854f
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-11-25 00:14:53 +0300

    I1-46 test: add new tests

--- apps/inctagram-gateway/test/testHelpers/auth.test.helper.ts
+++ apps/inctagram-gateway/test/testHelpers/auth.test.helper.ts
@@ -2,16 +2,20 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { endpoints } from '../../src/features/auth/api';
 import { CreateUserDto } from '../../src/features/user/dto';
+import { ConfirmationCodeDto } from '../../src/features/auth/dto';
+import { getGlobalPrefix, randomString } from '../utils/tests.utils';
 
 export class AuthTestHelper {
+  globalPrefix = getGlobalPrefix();
   constructor(private app: INestApplication) {}
 
   userDto(): CreateUserDto {
+    const password = `${randomString(10)}TU1!`;
     return {
-      username: 'new-test-user',
-      email: 'test@test.com',
-      password: 'newTestUser1!',
-      passwordConfirm: 'newTestUser1!',
+      username: randomString(10),
+      email: `${randomString(6)}@test.com`,
+      password: password,
+      passwordConfirm: password,
     };
   }
 
@@ -24,8 +28,22 @@ export class AuthTestHelper {
     const expectedCode = config.expectedCode ?? HttpStatus.CREATED;
 
     return request(this.app.getHttpServer())
-      .post('/api' + endpoints.registration())
+      .post(this.globalPrefix + endpoints.registration())
       .send(userDto)
+      .expect(expectedCode);
+  }
+
+  async confirmRegistration(
+    confirmDto: ConfirmationCodeDto,
+    config: {
+      expectedCode?: number;
+    } = {},
+  ) {
+    const expectedCode = config.expectedCode ?? HttpStatus.NO_CONTENT;
+
+    return request(this.app.getHttpServer())
+      .post(this.globalPrefix + endpoints.registrationConfirmation())
+      .send(confirmDto)
       .expect(expectedCode);
   }
 }
