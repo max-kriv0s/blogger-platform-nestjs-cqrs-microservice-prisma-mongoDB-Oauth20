096d91a8fca26d6b12f4a8b01191e234a9aa39e0
chlamydamanada <evil.cabaret@gmail.com>
2023-12-06 10:21:22 +0300

    I1-52: swagger: add for logout

---
+++ apps/inctagram-gateway/src/core/swagger/auth/auth.enum.ts
@@ -0,0 +1,11 @@
+export enum AuthSwagger {
+  login = 'Try login user to the system',
+  registration = 'Registration in the system. Email with confirmation code will be send to passed email address',
+  me = 'Returns information about current user',
+  confirm = 'Confirm registration',
+  resending = 'Resend confirmation registration email',
+  rT = 'Generate new pair of access and refresh tokens',
+  logout = 'In cookie client must send correct refresh token that will be revoked',
+  passRecovery = 'Password recovery via email confirmation. Email should be sent with recovery code inside',
+  newPass = 'Confirm password recovery',
+}
---
+++ apps/inctagram-gateway/src/core/swagger/swagger.constants.ts
@@ -0,0 +1,2 @@
+export const NO_CONTENT = 'No Content';
+export const UNAUTHORIZED = 'Unauthorized';
