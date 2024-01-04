639028340f45e2da0511df9d6851a1d69fed3bc3
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-01-04 12:18:04 +0300

    I1-101 feat: change UploadAvatarUseCase &&  DeleteAvatarUseCases

--- apps/inctagram-upload-files/src/files/application/use-cases/deleteAvatar.usecase.ts
+++ apps/inctagram-upload-files/src/files/application/use-cases/deleteAvatar.usecase.ts
@@ -1,17 +1,34 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { S3StorageAdapter } from '../../adapters';
+import { FileRepository } from '../../db/file.repository';
+import { AvatarDeleteResponse } from '@libs/contracts';
+import { Logger } from '@nestjs/common';
 
 export class DeleteAvatarCommand {
   constructor(public fileId: string) {}
 }
 
 @CommandHandler(DeleteAvatarCommand)
-export class DeleteAvatarUseCase
+export class DeleteAvatarUseCases
   implements ICommandHandler<DeleteAvatarCommand>
 {
-  constructor(private readonly fileStorageAdapter: S3StorageAdapter) {}
+  constructor(
+    private readonly fileStorageAdapter: S3StorageAdapter,
+    private readonly fileRepo: FileRepository,
+  ) {}
 
-  async execute({ fileId }: DeleteAvatarCommand) {
-    return this.fileStorageAdapter.deleteAvatar(fileId);
+  async execute({
+    fileId,
+  }: DeleteAvatarCommand): Promise<AvatarDeleteResponse> {
+    const result = { isSuccess: true };
+    const file = await this.fileRepo.findFileById(fileId);
+    if (!file) {
+      return result;
+    }
+
+    await this.fileStorageAdapter.deleteAvatar(file.url);
+    await this.fileRepo.deleteFile(file._id);
+
+    return result;
   }
 }
