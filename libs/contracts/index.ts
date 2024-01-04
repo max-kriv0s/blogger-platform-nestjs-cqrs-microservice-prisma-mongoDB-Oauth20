eb7f05229338202679c73347d5c9fcbb9782fd93
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-01-04 12:14:24 +0300

    I1-101 feat: add and change file contracts

--- libs/contracts/index.ts
+++ libs/contracts/index.ts
@@ -1,3 +1,6 @@
 export * from './user/avatar/avatarUploadRequest.dto';
 export * from './user/avatar/avatarUploadResponse.dto';
 export * from './user/avatar/avatarDeleteRequest.dto';
+export * from './user/avatar/avatarDeleteResponse.dto';
+export * from './user/file/fileUrlRequest.dto';
+export * from './user/file/fileUrlResponse.dto';
--- libs/contracts/user/avatar/avatarUploadResponse.dto.ts
+++ libs/contracts/user/avatar/avatarUploadResponse.dto.ts
@@ -1,4 +1,3 @@
 export class AvatarUploadResponse {
-  url: string;
   fileId: string;
 }
