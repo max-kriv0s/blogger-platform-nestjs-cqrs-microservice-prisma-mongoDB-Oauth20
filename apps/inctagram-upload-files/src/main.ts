729c4e6cf2d58fb6ed744c8e82c6ecc55a59d764
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-01-07 16:34:53 +0300

    build: init setting inctagram-upload-files

--- apps/inctagram-upload-files/src/main.ts
+++ apps/inctagram-upload-files/src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
     {
       transport: Transport.TCP,
       options: {
-        host: process.env.FILE_SERVICE_HOST,
-        port: parseInt(process.env.FILE_SERVICE_PORT) || undefined,
+        host: process.env.FILE_SERVICE_HOST || '0.0.0.0',
+        port: Number(process.env.FILE_SERVICE_PORT || '3196'),
       },
     },
   );
--- apps/inctagram-upload-files/test/app.e2e-spec.ts
+++ apps/inctagram-upload-files/test/app.e2e-spec.ts
@@ -1,20 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestMicroservice } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { Transport } from '@nestjs/microservices';
 
 describe('InctagramUploadFilesController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestMicroservice;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = await moduleFixture.createNestMicroservice({
+      transport: Transport.TCP,
+      options: {
+        host: process.env.FILE_SERVICE_HOST || '0.0.0.0',
+        port: Number(process.env.FILE_SERVICE_PORT || '3196'),
+      },
+    });
+    await app.listen();
   });
 
   it('/ (GET)', () => {
     expect(2).toBe(2);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
