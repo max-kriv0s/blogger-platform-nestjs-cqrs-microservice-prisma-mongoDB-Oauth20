82dcf145d0b35e1daf52df24314deeb966d9a190
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-01-20 23:26:56 +0300

    I2-19 feat: change post model

---
+++ prisma/migrations/20240120201103_change_post_model/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `posts` table. All the data in the column will be lost.
+  - Added the required column `description` to the `posts` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "posts" RENAME COLUMN "title" TO "description";
