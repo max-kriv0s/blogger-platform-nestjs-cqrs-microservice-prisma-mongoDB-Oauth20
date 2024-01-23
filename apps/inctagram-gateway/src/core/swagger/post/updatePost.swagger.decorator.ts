5e4348a3c75b7ab7bf4d18dbe1e58f2cc8b57c43
Nazim <nazim86@gmail.com>
2024-01-23 17:09:26 +0400

    403 response in swagger

--- apps/inctagram-gateway/src/core/swagger/post/updatePost.swagger.decorator.ts
+++ apps/inctagram-gateway/src/core/swagger/post/updatePost.swagger.decorator.ts
@@ -5,16 +5,21 @@ import {
   ApiNotFoundResponse,
   ApiUnauthorizedResponse,
   ApiBadRequestResponse,
+  ApiForbiddenResponse,
 } from '@nestjs/swagger';
 import { ApiErrorResponse, BadRequestResponse } from '../../responses';
 import { BAD_REQUEST, NOT_FOUND } from '../swagger.constants';
 import { ResponsePostDto } from '@gateway/src/features/post/responses/responsePost.dto';
+import { NOT_PERMITTED } from '@gateway/src/core/swagger/swagger.constants';
 
 export function UpdatePostSwaggerDecorator() {
   return applyDecorators(
     ApiUnauthorizedResponse({ type: ApiErrorResponse }),
     ApiOperation({
       summary: 'Update post description',
+    }),
+    ApiForbiddenResponse({
+      description: NOT_PERMITTED,
     }),
     ApiOkResponse({ type: ResponsePostDto }),
     ApiNotFoundResponse({ type: ApiErrorResponse, description: NOT_FOUND }),
