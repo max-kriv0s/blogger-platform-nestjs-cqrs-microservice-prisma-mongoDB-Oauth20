8eb13a88ce4a1fcbc2ddb845f5713d227499833a
Nazim <nazim86@gmail.com>
2024-01-23 17:24:07 +0400

    test for not update with wrong id

--- apps/inctagram-gateway/src/features/post/application/use-cases/updatePost.usecase.spec.ts
+++ apps/inctagram-gateway/src/features/post/application/use-cases/updatePost.usecase.spec.ts
@@ -7,12 +7,15 @@ import {
   UpdatePostUseCase,
 } from '@gateway/src/features/post/application/use-cases/updatePost.usecase';
 import { randomString } from '@gateway/test/e2e.tests/utils/tests.utils';
-import { Post } from '@prisma/client';
+import { Post, User } from '@prisma/client';
+import { ERROR_POST_NOT_FOUND } from '@gateway/src/features/post/post.constants';
 
 describe('UpdatePostUseCase', () => {
   let module: TestingModule;
   let prismaService: PrismaService;
   let useCase: UpdatePostUseCase;
+  let payload: UpdatePostDto;
+  let user: User;
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
@@ -37,7 +40,7 @@ describe('UpdatePostUseCase', () => {
         hashPassword: '12345',
       };
 
-      const user = await prismaService.user.create({ data: userDto });
+      user = await prismaService.user.create({ data: userDto });
 
       const postDto = {
         description: 'new post created',
@@ -47,7 +50,7 @@ describe('UpdatePostUseCase', () => {
 
       const post: Post = await prismaService.post.create({ data: postDto });
 
-      const payload: UpdatePostDto = {
+      payload = {
         description: 'post updated',
       };
 
@@ -62,6 +65,15 @@ describe('UpdatePostUseCase', () => {
       });
 
       expect(updatedPost.description).toEqual('post updated');
+    });
+
+    it('should not update post description with wrong id', async () => {
+      const updateResult = await useCase.execute(
+        new UpdatePostCommand('dfddf', payload, user.id),
+      );
+
+      expect(updateResult.isSuccess).toBe(false);
+      expect(updateResult.err.message).toBe(ERROR_POST_NOT_FOUND);
     });
   });
 });
