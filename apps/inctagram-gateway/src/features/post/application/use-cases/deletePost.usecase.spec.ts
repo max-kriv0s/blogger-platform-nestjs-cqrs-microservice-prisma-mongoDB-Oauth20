a39c990f47ff6aacb967644051b6f4f3af04080f
Nazim <nazim86@gmail.com>
2024-01-26 02:43:59 +0400

    delete post use case test update

--- apps/inctagram-gateway/src/features/post/application/use-cases/deletePost.usecase.spec.ts
+++ apps/inctagram-gateway/src/features/post/application/use-cases/deletePost.usecase.spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '@gateway/src/core/prisma/prisma.servise';
 import { AppModule } from '@gateway/src/app.module';
-import { UpdatePostDto } from '@gateway/src/features/post/dto/updatePost.dto';
-import { UpdatePostCommand } from '@gateway/src/features/post/application/use-cases/updatePost.usecase';
 import { randomString } from '@gateway/test/e2e.tests/utils/tests.utils';
 import { Post, User } from '@prisma/client';
 import {
   DeletePostCommand,
   DeletePostUseCase,
 } from '@gateway/src/features/post/application/use-cases/deletePost.usecase';
+import {
+  ERROR_NOT_PERMITTED,
+  ERROR_POST_NOT_FOUND,
+} from '@gateway/src/features/post/post.constants';
 
 describe('DeletePostUseCase', () => {
   let module: TestingModule;
@@ -50,42 +52,45 @@ describe('DeletePostUseCase', () => {
 
       post = await prismaService.post.create({ data: postDto });
 
-      const payload: UpdatePostDto = {
-        description: 'post updated',
-      };
-
-      const updateResult = await useCase.execute(
-        new UpdatePostCommand(post.id, payload, user.id),
+      const deleteResult = await useCase.execute(
+        new DeletePostCommand(post.id, 'different_user.id'),
       );
 
-      expect(updateResult.isSuccess).toBe(true);
+      expect(deleteResult.err.message).toBe(ERROR_NOT_PERMITTED);
 
+      const isPostExists = await prismaService.post.findUnique({
+        where: { id: post.id },
+      });
+
+      expect(isPostExists).not.toBe(null);
+    });
+
+    it('should not delete post with wrong post id', async () => {
+      const deleteResult = await useCase.execute(
+        new DeletePostCommand('wrong post.id', user.id),
+      );
+
+      expect(deleteResult.err.message).toBe(ERROR_POST_NOT_FOUND);
+
+      const isPostExists = await prismaService.post.findUnique({
+        where: { id: post.id },
+      });
+
+      expect(isPostExists).not.toBe(null);
+    });
+
+    it('Should delete post', async () => {
       const deleteResult = await useCase.execute(
         new DeletePostCommand(post.id, user.id),
       );
 
-      const updatedPost = await prismaService.post.findUnique({
+      expect(deleteResult.isSuccess).toBe(true);
+
+      const isPostExists = await prismaService.post.findUnique({
         where: { id: post.id },
       });
 
-      expect(updatedPost).toBe(null);
-
-      //expect(deleteResult.err.message).toBe(ERROR_NOT_PERMITTED);
-
-      // await prismaService.post.delete({
-      //   where: { id: post.id },
-      // });
-      //
-      // const updatedPost = await prismaService.post.findUnique({
-      //   where: { id: post.id },
-      // });
-      //
-      // expect(updatedPost).toBe(null);
+      expect(isPostExists).toBe(null);
     });
-
-    // it('should not delete post with wrong post id', async () => {
-    //
-    //
-    // });
   });
 });
